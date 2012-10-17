d550a1cd18a209b894ced762cf44a2b9e93e0371
Jim Paterson <James.Paterson@gcu.ac.uk>
2012-10-17 23:14:13 +0100

    initial commit

---
+++ RazorSyntaxDemo/RazorSyntaxDemo/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RazorSyntaxDemo.Models;
+
+namespace RazorSyntaxDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            Product product = new Product { ProductID = 1, ProductName = "Banana", Description = "Yellow", Price = 9.99M };
+            return View(product);
+        }
+
+        public ActionResult LayoutDemo()
+        {
+            return View();
+        }
+
+    }
+}
